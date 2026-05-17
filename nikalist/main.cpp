3c5cfb64c82652bcf78bbf21a16e9b482c11cb95
Nika Lipinska <nikaowl2107@gmail.com>
2026-05-17 19:30:17 +0300

    Initial Commit

---
+++ nikalist/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  nikalist
+//
+//  Created by MacBook Air on 17.05.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
