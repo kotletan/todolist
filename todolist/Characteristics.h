824e989269423681b6cb32c340e80e88edbcd2ae
Nika Lipinska <nikaowl2107@gmail.com>
2026-06-04 20:05:22 +0300

    Final project edit

---
+++ todolist/Characteristics.h
@@ -0,0 +1,17 @@
+#pragma once
+
+typedef unsigned int   uint;
+typedef unsigned short ushort;
+
+struct Task
+{
+    char*  name;
+    char*  description;
+    ushort day;
+    ushort month;
+    ushort year;
+    ushort week;
+    ushort time;
+    ushort numb;
+};
+
